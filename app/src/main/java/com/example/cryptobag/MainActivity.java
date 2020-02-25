98396261869b09b875e38b39d2566cebefd70b5e
matt_ha <30426868+MatthewHa3@users.noreply.github.com>
2020-02-25 11:16:33 +1100

    Initial commit

--- app/src/main/java/com/example/cryptobag/MainActivity.java
+++ app/src/main/java/com/example/cryptobag/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.cryptobag;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    public static final String EXTRA_MESSAGE = "au.edu.unsw.infs3634.beers.MESSAGE";
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_main);
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                launchDetailActivity("Hello");
+            }
+        });
+    }
+
+
+    private void launchDetailActivity(String message){
+        Intent intent = new Intent( this, DetailActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
